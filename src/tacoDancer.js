8e89ed58f675d0077463c638c16bdf8f320a952b
jessehang <hangtimee@gmail.com>
2019-02-26 17:50:23 -0800

    Implemented taco dancer with jump on hover

--- src/tacoDancer.js
+++ src/tacoDancer.js
@@ -1,7 +1,12 @@
 var makeTacoDancer = function(top, left, timeBetweenSteps) {
     makeDancer.call(this, top, left, timeBetweenSteps);
-    // this.$node = $('<span class="dancer"></span>');
     this.$node.addClass('dancer3');
+    this.$node.append($('<img src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/apple/48/taco_1f32e.png">'));
+    //this.$node.addClass('dancer3');
+    this.$node.on("mouseover",function() {
+      // $('.dancer3').animate({left: '100px'}, 1000, 'linear');
+      $('.dancer3').addClass('jump');
+    });
   };
   
 makeTacoDancer.prototype = Object.create(makeDancer.prototype);
